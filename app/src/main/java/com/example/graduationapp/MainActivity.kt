d4cad8bd8f842e320b1b80ed8eabda4a26a73543
tasneem <tasneemmostafa777@gmail.com>
2021-06-06 00:14:05 +0200

    curved navigation

--- app/src/main/java/com/example/graduationapp/MainActivity.kt
+++ app/src/main/java/com/example/graduationapp/MainActivity.kt
@@ -15,22 +15,36 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main2)
-//        val navView: BottomNavigationView = findViewById(R.id.nav_view)
-//
-//        val navController = findNavController(R.id.nav_host_fragment)
-//        // Passing each menu ID as a set of Ids because each
-//        // menu should be considered as top level destinations.
-//        val appBarConfiguration = AppBarConfiguration(setOf(
-//                R.id.navigation_home, R.id.navigation_category, R.id.navigation_me))
-//        //setupActionBarWithNavController(navController, appBarConfiguration)
-//        SharedPref.createPrefObject(this)
-//        navView.setupWithNavController(navController)
+        setContentView(R.layout.activity_main)
+        //val navView: MeowBottomNavigation = findViewById(R.id.nav_view)
+
+        val navController = findNavController(R.id.nav_host_fragment)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(setOf(
+                R.id.navigation_home, R.id.navigation_category, R.id.navigation_me))
+        //setupActionBarWithNavController(navController, appBarConfiguration)
+        SharedPref.createPrefObject(this)
+        //navView.setupWithNavController(navController)
 
         bottomNavigationView = findViewById(R.id.bottom_navigation)
         bottomNavigationView.add(MeowBottomNavigation.Model(1,R.drawable.com_facebook_button_icon))
         bottomNavigationView.add(MeowBottomNavigation.Model(2,R.drawable.ic_favorite))
         bottomNavigationView.add(MeowBottomNavigation.Model(3,R.drawable.ic_action_favorite))
+
+        bottomNavigationView.setOnClickMenuListener {
+            when(it.id) {
+                1->{
+                    navController.navigate(R.id.navigation_home)
+                }
+                2->{
+                    navController.navigate(R.id.navigation_category)
+                }
+                3->{
+                    navController.navigate(R.id.navigation_me)
+                }
+            }
+        }
 
 
     }
