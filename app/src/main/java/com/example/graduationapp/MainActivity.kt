a04f1799b3f825810ce60c40c66b269f61636ae3
mohamed-abdallah5012 <mohamed.abdallah50125012@gmail.com>
2021-06-05 23:16:05 +0200

    add curved bottom navigation

--- app/src/main/java/com/example/graduationapp/MainActivity.kt
+++ app/src/main/java/com/example/graduationapp/MainActivity.kt
@@ -7,21 +7,31 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.etebarian.meowbottomnavigation.MeowBottomNavigation
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var  bottomNavigationView :MeowBottomNavigation
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+        setContentView(R.layout.activity_main2)
+//        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+//
+//        val navController = findNavController(R.id.nav_host_fragment)
+//        // Passing each menu ID as a set of Ids because each
+//        // menu should be considered as top level destinations.
+//        val appBarConfiguration = AppBarConfiguration(setOf(
+//                R.id.navigation_home, R.id.navigation_category, R.id.navigation_me))
+//        //setupActionBarWithNavController(navController, appBarConfiguration)
+//        SharedPref.createPrefObject(this)
+//        navView.setupWithNavController(navController)
 
-        val navController = findNavController(R.id.nav_host_fragment)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(setOf(
-                R.id.navigation_home, R.id.navigation_category, R.id.navigation_me))
-        //setupActionBarWithNavController(navController, appBarConfiguration)
-        SharedPref.createPrefObject(this)
-        navView.setupWithNavController(navController)
+        bottomNavigationView = findViewById(R.id.bottom_navigation)
+        bottomNavigationView.add(MeowBottomNavigation.Model(1,R.drawable.com_facebook_button_icon))
+        bottomNavigationView.add(MeowBottomNavigation.Model(2,R.drawable.ic_favorite))
+        bottomNavigationView.add(MeowBottomNavigation.Model(3,R.drawable.ic_action_favorite))
+
+
     }
 }
