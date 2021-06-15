a5e1ad53518dcdc2ba99b038f73772ab2ebaa2ef
mohamed-abdallah5012 <mohamed.abdallah50125012@gmail.com>
2021-06-15 13:29:26 +0200

    address-book

--- app/src/main/java/com/example/graduationapp/ui/addressbook/AdressBook.kt
+++ app/src/main/java/com/example/graduationapp/ui/addressbook/AdressBook.kt
@@ -3,10 +3,30 @@ package com.example.graduationapp.ui.addressbook
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.graduationapp.R
+import com.example.graduationapp.data.Addresse
+import com.example.graduationapp.ui.addressbook.adapater.AddressAdapter
 
-class AddressBook : AppCompatActivity() {
+class AddressBook : AppCompatActivity(),AddressAdapter.OnClickAddressListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_adress_book)
+
+
+    }
+
+    override fun onDeleteAddressClick(item: Addresse) {
+
+    }
+
+    override fun onNameAddressClick(item: Addresse) {
+
+    }
+
+    override fun onAddressClick(item: Addresse) {
+
+    }
+
+    override fun onCheckBoxChange(item: Addresse) {
+
     }
 }
